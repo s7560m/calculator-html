07a63eadde02011a5138f2d14030efca27177f88
Hayden Hoffman <haydenhoffman@gmail.com>
2022-08-24 18:30:00 -0400

    working calculator with bugs

--- calculator.js
+++ calculator.js
@@ -1,8 +1,17 @@
 let resultNumber = document.getElementById('result-number')
 
+let inputArray = []
+let operationType = ""
+let operationFlag = false;
+
 function inputNumber(number) {
 
     const str_number = number.toString();
+
+    if (operationFlag) {
+        resultNumber.innerHTML = "0";
+        operationFlag = false;
+    }
 
     // when it's zero, we simply replace with the input number
     if (resultNumber.innerHTML === "0") {
@@ -14,10 +23,28 @@ function inputNumber(number) {
     }
 }
 
+// When the AC button is clicked, clear the input and the array and operation types
 function clearInput() {
     resultNumber.innerHTML = "0";
+    inputArray = [];
+    operationType = "";
 }
 
 function callOperation(operation_type) {
-    
+
+    if (operation_type === "=") {
+        inputArray.push(parseInt(resultNumber.innerHTML))
+        let result;
+        if (operationType === "+") result = inputArray[0] + inputArray[1];
+        if (operationType === "-") result = inputArray[0] - inputArray[1];
+        if (operationType === "*") result = inputArray[0] * inputArray[1];
+        if (operationType === "/") result = (inputArray[0] / inputArray[1]).toFixed(6);
+        resultNumber.innerHTML = result.toString();
+        return;
+    }
+
+    inputArray.push(parseInt(resultNumber.innerHTML))
+    operationType = operation_type;
+    operationFlag = true;
+
 }
